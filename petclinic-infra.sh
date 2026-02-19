ca4e21d066d0ceb5d96a8b00135c676967de4fed
Denis Bell <denisdbell@gmail.com>
2026-02-18 19:48:35 -0500

    Update petclinic-infra.sh

--- petclinic-infra.sh
+++ petclinic-infra.sh
@@ -1,25 +1,5 @@
-LOC="westus3" 
-
-az group create --name rg-dev --location $LOC
-
-az deployment group create \
-  --name DeployDev \
-  --resource-group rg-dev \
-  --template-file petclinic-infra.json \
-  --parameters environmentName=dev
-
-az group create --name rg-testing --location $LOC
-
-az deployment group create \
-  --name DeployDev \
-  --resource-group rg-testing \
-  --template-file petclinic-infra.json \
-  --parameters environmentName=test
-
-az group create --name rg-prod --location $LOC
-
-az deployment group create \
-  --name DeployDev \
-  --resource-group rg-prod \
-  --template-file petclinic-infra.json \
-  --parameters environmentName=prod
+az deployment sub create \
+  --name petclinic-sub-deploy \
+  --location westus3 \
+  --template-file main.json \
+  --parameters region=westus3
