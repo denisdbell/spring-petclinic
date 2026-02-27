3a8973ee460dc222a0618bab02972e7427e19671
Denis Bell <denisdbell@gmail.com>
2026-02-26 19:58:53 -0500

    Update deployment name from petclinic-aks-deploy3 to petclinic-aks-deploy4

--- aks-infra.sh
+++ aks-infra.sh
@@ -1,7 +1,7 @@
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/aks_rsa -N ""
 
 az deployment sub create \
-  --name petclinic-aks-deploy3 \
+  --name petclinic-aks-deploy4 \
   --location westus3 \
   --template-file aks-infra.json \
   --parameters sshRSAPublicKey="$(cat ~/.ssh/aks_rsa.pub)" \
